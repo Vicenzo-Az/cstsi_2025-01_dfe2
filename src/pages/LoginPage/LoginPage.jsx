4df0e924b61252a8739fbd831e5f5cc81847cce1
Vicenzo-Az <vicenzo.net@gmail.com>
2025-06-29 20:41:55 -0300

    refactor

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -15,7 +15,7 @@ const LoginPage = () => {
     setError('');
 
     try {
-      const response = await fetch('http://localhost:3001/auth/login', {
+      const response = await fetch('http://localhost:3000/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
